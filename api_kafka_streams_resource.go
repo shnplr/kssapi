1280d379653feea0831ea12be1ac49c92cdbd079
Paul Strachan <12369009+shnplr@users.noreply.github.com>
2023-01-20 09:14:56 +1100

    Delete streams

--- api_kafka_streams_resource.go
+++ api_kafka_streams_resource.go
@@ -178,6 +178,166 @@ func (a *KafkaStreamsResourceApiService) ApisKafkaStreamsV1NamespacesNamespaceSt
 	return localVarReturnValue, localVarHTTPResponse, nil
 }
 
+type ApiApisKafkaStreamsV1NamespacesNamespaceStreamsNameDeleteRequest struct {
+	ctx context.Context
+	ApiService *KafkaStreamsResourceApiService
+	name string
+	namespace string
+}
+
+func (r ApiApisKafkaStreamsV1NamespacesNamespaceStreamsNameDeleteRequest) Execute() (*KafkaStreams, *http.Response, error) {
+	return r.ApiService.ApisKafkaStreamsV1NamespacesNamespaceStreamsNameDeleteExecute(r)
+}
+
+/*
+ApisKafkaStreamsV1NamespacesNamespaceStreamsNameDelete Method for ApisKafkaStreamsV1NamespacesNamespaceStreamsNameDelete
+
+ @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+ @param name
+ @param namespace
+ @return ApiApisKafkaStreamsV1NamespacesNamespaceStreamsNameDeleteRequest
+*/
+func (a *KafkaStreamsResourceApiService) ApisKafkaStreamsV1NamespacesNamespaceStreamsNameDelete(ctx context.Context, name string, namespace string) ApiApisKafkaStreamsV1NamespacesNamespaceStreamsNameDeleteRequest {
+	return ApiApisKafkaStreamsV1NamespacesNamespaceStreamsNameDeleteRequest{
+		ApiService: a,
+		ctx: ctx,
+		name: name,
+		namespace: namespace,
+	}
+}
+
+// Execute executes the request
+//  @return KafkaStreams
+func (a *KafkaStreamsResourceApiService) ApisKafkaStreamsV1NamespacesNamespaceStreamsNameDeleteExecute(r ApiApisKafkaStreamsV1NamespacesNamespaceStreamsNameDeleteRequest) (*KafkaStreams, *http.Response, error) {
+	var (
+		localVarHTTPMethod   = http.MethodDelete
+		localVarPostBody     interface{}
+		formFiles            []formFile
+		localVarReturnValue  *KafkaStreams
+	)
+
+	localBasePath, err := a.client.cfg.ServerURLWithContext(r.ctx, "KafkaStreamsResourceApiService.ApisKafkaStreamsV1NamespacesNamespaceStreamsNameDelete")
+	if err != nil {
+		return localVarReturnValue, nil, &GenericOpenAPIError{error: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/apis/kafka.streams/v1/namespaces/{namespace}/streams/{name}"
+	localVarPath = strings.Replace(localVarPath, "{"+"name"+"}", url.PathEscape(parameterToString(r.name, "")), -1)
+	localVarPath = strings.Replace(localVarPath, "{"+"namespace"+"}", url.PathEscape(parameterToString(r.namespace, "")), -1)
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := url.Values{}
+	localVarFormParams := url.Values{}
+
+	// to determine the Content-Type header
+	localVarHTTPContentTypes := []string{}
+
+	// set Content-Type header
+	localVarHTTPContentType := selectHeaderContentType(localVarHTTPContentTypes)
+	if localVarHTTPContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHTTPContentType
+	}
+
+	// to determine the Accept header
+	localVarHTTPHeaderAccepts := []string{"application/json"}
+
+	// set Accept header
+	localVarHTTPHeaderAccept := selectHeaderAccept(localVarHTTPHeaderAccepts)
+	if localVarHTTPHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
+	}
+	req, err := a.client.prepareRequest(r.ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, formFiles)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.client.callAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := ioutil.ReadAll(localVarHTTPResponse.Body)
+	localVarHTTPResponse.Body.Close()
+	localVarHTTPResponse.Body = ioutil.NopCloser(bytes.NewBuffer(localVarBody))
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: localVarHTTPResponse.Status,
+		}
+		if localVarHTTPResponse.StatusCode == 400 {
+			var v ApiStatus
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+            		newErr.error = formatErrorMessage(localVarHTTPResponse.Status, &v)
+            		newErr.model = v
+			return localVarReturnValue, localVarHTTPResponse, newErr
+		}
+		if localVarHTTPResponse.StatusCode == 401 {
+			var v ApiStatus
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+            		newErr.error = formatErrorMessage(localVarHTTPResponse.Status, &v)
+            		newErr.model = v
+			return localVarReturnValue, localVarHTTPResponse, newErr
+		}
+		if localVarHTTPResponse.StatusCode == 404 {
+			var v ApiStatus
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+            		newErr.error = formatErrorMessage(localVarHTTPResponse.Status, &v)
+            		newErr.model = v
+			return localVarReturnValue, localVarHTTPResponse, newErr
+		}
+		if localVarHTTPResponse.StatusCode == 500 {
+			var v ApiStatus
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+            		newErr.error = formatErrorMessage(localVarHTTPResponse.Status, &v)
+            		newErr.model = v
+			return localVarReturnValue, localVarHTTPResponse, newErr
+		}
+		if localVarHTTPResponse.StatusCode == 503 {
+			var v ApiStatus
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+            		newErr.error = formatErrorMessage(localVarHTTPResponse.Status, &v)
+            		newErr.model = v
+			return localVarReturnValue, localVarHTTPResponse, newErr
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.client.decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+	if err != nil {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
+
 type ApiApisKafkaStreamsV1NamespacesNamespaceStreamsNameGetRequest struct {
 	ctx context.Context
 	ApiService *KafkaStreamsResourceApiService
